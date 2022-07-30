96ddf0f29bb9e5ced71c330e146968826f587027
Gildas <gildas.lormeau@gmail.com>
2022-07-30 15:22:13 +0200

    escape white spaces

--- mac/chrome/install.sh
+++ mac/chrome/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p ~/Library/Application Support/Google/Chrome/NativeMessagingHosts/
-jq '.path= "'$PWD'/single-file-companion.sh"' singlefile_companion.json > ~/Library/Application Support/Google/Chrome/NativeMessagingHosts/singlefile_companion.json
+mkdir -p ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/
+jq '.path= "'$PWD'/single-file-companion.sh"' singlefile_companion.json > ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/singlefile_companion.json
--- mac/chrome/uninstall.sh
+++ mac/chrome/uninstall.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -f ~/Library/Application Support/Google/Chrome/NativeMessagingHosts/singlefile_companion.json
+rm -f ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/singlefile_companion.json
